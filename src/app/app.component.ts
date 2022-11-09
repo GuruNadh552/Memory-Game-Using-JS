8e3d848c1fb0f6405ac1b758af2ae395b50d5b29
Gurunadh552 <gurunadhpukkalla@gmail.com>
2022-11-09 10:14:24 +0530

    Commented Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'memory-game';
+  // title = 'memory-game';
   isStart: boolean = false;
   gameIcons: any[] = [];
   firstCard: any;
